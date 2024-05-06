73ff7bb310ec1607fe9495390670bad8117c6020
kiranbabup <kiran12146@gmail.com>
2024-05-06 17:56:56 +0530

    live test 3

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import HomeLayout from "./layout/HomeLayout";
 
 const Router = () => {
     return (
         <Routes>
-            <Route path="/" element={<HomeLayout />} />
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<HomeLayout />} />
         </Routes>
   );
 };
